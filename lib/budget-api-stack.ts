00702597bca30deb6c10c6ee442c13b9a3916ac8
lmatyila <lmatyila@mit.edu>
2023-08-25 18:49:20 +0200

    frist commit

--- lib/budget-api-stack.ts
+++ lib/budget-api-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as path from 'path';
 
 export class BudgetApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const apiGateway = new apigateway.RestApi(this, "BudgetAPIGateway", {    
+    })
+   
+    const budgetLambda = new lambda.Function(this, "BudgetLambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, "../","assets","/lambda","/budget")),
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BudgetApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    apiGateway.root.addResource('budgets')
+      .addMethod('GET', new apigateway.LambdaIntegration(budgetLambda));
+    
+    new dynamodb.Table(this, "BudgetsTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+    
+    
+   
+
   }
 }
